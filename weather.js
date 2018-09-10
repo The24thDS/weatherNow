c441c6ae5e4fc057ef03faed805ac4f8683a1ed3
David Sima <davidsima@windowslive.com>
2018-09-10 22:04:25 +0300

    Added basic search functionality

--- weather.js
+++ weather.js
@@ -1,18 +1,19 @@
 const api = `a6825f71c27c7eeae7da4b299f840fc4`;
-let units = `metric`;
-const url = `https://api.openweathermap.org/data/2.5/weather?id=6697993&units=${units}&appid=${api}`;
-const city = document.getElementById(`location`);
-const image = document.getElementById(`icon`);
-const description = document.getElementById(`description`);
-const temperature = document.querySelector(`#temperature > p`);
-const humidity = document.querySelector(`#humidity > p`);
-const sunrise = document.querySelector(`#sunrise > p`);
-const sunset = document.querySelector(`#sunset > p`);
-const wind = document.querySelector(`#wind > p`);
-const direction = document.querySelector(`#direction > p`);
-const visibility = document.querySelector(`#visibility > p`);
-const cloudiness = document.querySelector(`#cloudiness > p`);
-const pressure = document.querySelector(`#pressure > p`);
+const   weatherDiv = document.getElementById(`weather`);
+        city = document.getElementById(`location`),
+        image = document.getElementById(`icon`),
+        description = document.getElementById(`description`),
+        temperature = document.querySelector(`#temperature > p`),
+        humidity = document.querySelector(`#humidity > p`),
+        sunrise = document.querySelector(`#sunrise > p`),
+        sunset = document.querySelector(`#sunset > p`),
+        wind = document.querySelector(`#wind > p`),
+        direction = document.querySelector(`#direction > p`),
+        visibility = document.querySelector(`#visibility > p`),
+        cloudiness = document.querySelector(`#cloudiness > p`),
+        pressure = document.querySelector(`#pressure > p`),
+        searchBar = document.getElementById(`searchbar`),
+        notFound = document.getElementById(`nope`);
 const icons = {
     2: "008-storm.svg",
     3: "016-drizzle.svg",
@@ -65,18 +66,41 @@ const weatherIcon = (cod, icon) => {
         return icons[781];
     return icons[Math.floor(cod/100)];
 }
+const fetchWeather = (query) => {
+    let units = `metric`;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&units=${units}&appid=${api}`;
+    console.log(url);
+    fetch(url).then(result => result.json()).then(json => {
+        if(json.cod===200){
+            weatherDiv.style.display = `flex`;
+            nope.style.display = `none`;
+            city.textContent = `${json.name}, ${json.sys.country}`;
+            image.src = `icons/svg/${weatherIcon(json.weather[0].id, json.weather[0].icon[2])}`;
+            description.textContent = json.weather[0].description;
+            temperature.innerHTML = `${json.main.temp} &deg;C`;
+            humidity.textContent = `${json.main.humidity}%`;
+            sunrise.textContent = humanTime(json.sys.sunrise);
+            sunset.textContent = humanTime(json.sys.sunset);
+            wind.textContent = `${json.wind.speed} m/s`;
+            direction.textContent = json.wind.deg ? degreeToCardinal(json.wind.deg): `no data`;
+            visibility.textContent = json.visibility ? `${json.visibility} m` : `no data`;
+            cloudiness.textContent = `${json.clouds.all}%`;
+            pressure.textContent = `${json.main.pressure} hPa`;
+        }
+        if(json.cod==="404")
+        {
+            weatherDiv.style.display = `none`;
+            nope.style.display = `flex`;
+        }
+    }).catch(console.log());
+}
 
-fetch(url).then(result => result.json()).then(json => {
-    city.textContent = `${json.name}, ${json.sys.country}`;
-    image.src = `icons/svg/${weatherIcon(json.weather[0].id, json.weather[0].icon[2])}`;
-    description.textContent = json.weather[0].description;
-    temperature.innerHTML = `${json.main.temp} &deg;C`;
-    humidity.textContent = `${json.main.humidity}%`;
-    sunrise.textContent = humanTime(json.sys.sunrise);
-    sunset.textContent = humanTime(json.sys.sunset);
-    wind.textContent = `${json.wind.speed} m/s`;
-    direction.textContent = degreeToCardinal(json.wind.deg);
-    visibility.textContent =`${json.visibility} m`;
-    cloudiness.textContent = `${json.clouds.all}%`;
-    pressure.textContent = `${json.main.pressure} hPa`;
-})
+searchBar.addEventListener('keypress', (e) => {
+    let key = e.which || e.keyCode;
+    if (key === 13) {
+        console.log(e.target.value);
+        fetchWeather(e.target.value);
+    }
+});
+
+fetchWeather("London");
